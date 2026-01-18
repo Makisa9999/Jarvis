e8ef700b101591110c76a71e5e44dcb8604c5395
Mario Jovanovic <mario@jovanovic.in.rs>
2026-01-18 15:55:09 -0500

    Testing SSH

--- python.py
+++ python.py
@@ -6,3 +6,6 @@ print("Available Input Microphone Devices...")
 for i in range(p.get_device_count()):
     dev_info = p.get_device_info_by_index(i)
     print(f"{i}: {dev_info['name']}")
+
+
+# Mario
